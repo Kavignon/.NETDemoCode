ba0e6e708bb186c4ce7479d510765e15ebfd3714
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-02-07 21:24:43 -0500

    Add initial projects for workshop
    There is a jumpstart to quickly understand what is F# and quickly grasp the syntax + a comparison of implementation for both Rock-Paper-Scissors and a calculator (C# v. F#)

---
+++ ScalingDevSkillsWithF#/SimplicityBringer/Rock-Paper-Scissors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
