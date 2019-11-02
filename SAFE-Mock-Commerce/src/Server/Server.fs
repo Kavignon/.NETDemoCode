8a10de45035c015dc791abb867ce3b739917b516
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-02 09:38:03 -0400

    Created SAFE environment
    The setup with the SAFE Template comes with the following: Saturn (default server back-end hosting platform), Fulma-basic template (a CSS framework wrapper for react in F#), Fable.Remote (easy to use communication layer between the client and the back-end server) and resources to deploy the app to Azure.

---
+++ SAFE-Mock-Commerce/src/Server/Server.fs
@@ -0,0 +1,48 @@
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.DependencyInjection
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Saturn
+open Shared
+
+open Fable.Remoting.Server
+open Fable.Remoting.Giraffe
+open Microsoft.WindowsAzure.Storage
+
+let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+
+let publicPath = tryGetEnv "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
+let storageAccount = tryGetEnv "STORAGE_CONNECTIONSTRING" |> Option.defaultValue "UseDevelopmentStorage=true" |> CloudStorageAccount.Parse
+
+let port =
+    "SERVER_PORT"
+    |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
+
+let counterApi = {
+    initialCounter = fun () -> async { return { Value = 42 } }
+}
+
+let webApp =
+    Remoting.createApi()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.fromValue counterApi
+    |> Remoting.buildHttpHandler
+
+let configureAzure (services:IServiceCollection) =
+    tryGetEnv "APPINSIGHTS_INSTRUMENTATIONKEY"
+    |> Option.map services.AddApplicationInsightsTelemetry
+    |> Option.defaultValue services
+
+let app = application {
+    url ("http://0.0.0.0:" + port.ToString() + "/")
+    use_router webApp
+    memory_cache
+    use_static publicPath
+    service_config configureAzure
+    use_gzip
+}
+
+run app
