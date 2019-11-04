b1b9c0842ad2a7b19d9b47fd6a4637dca4396d8a
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-03 22:30:04 -0500

    Simplified product sequence hydration.

--- SAFE-Mock-Commerce/src/Server/CatalogApiWorkflows.fs
+++ SAFE-Mock-Commerce/src/Server/CatalogApiWorkflows.fs
@@ -1,27 +1,13 @@
 module CatalogApiWorkflows
 
-open System
-
 open Shared
 open DtoToDomainMapper
 
-let getHeadphonesFromCatalogue (headphoneDtos: ProductCatalogue.Headphone seq) productList =
-    headphoneDtos
-    |> Seq.map(fun x -> convertDtoToStoreProduct (HeadphoneDto x))
-    |> Seq.append productList
+let hydrateProductSeq (storeItems: ProductCatalogue.Items)  =
+    let headphones = storeItems.Headphones |> Seq.map (HeadphoneDto >> convertDtoToStoreProduct)
+    let computers = storeItems.Computers |> Seq.map (ComputerDto >> convertDtoToStoreProduct)
+    let books = storeItems.Books |> Seq.map (ReadingMaterialDto >> convertDtoToStoreProduct)
 
-let getComputersFromCatalogue (headphoneDtos: ProductCatalogue.Computer seq) productList =
-    headphoneDtos
-    |> Seq.map(fun x -> convertDtoToStoreProduct (ComputerDto x))
-    |> Seq.append productList
-
-let getBooksFromCatalogue (headphoneDtos: ProductCatalogue.Book seq) productList =
-    headphoneDtos
-    |> Seq.map(fun x -> convertDtoToStoreProduct (ReadingMaterialDto x))
-    |> Seq.append productList
-
-let loadStoreProducts (storeItems: ProductCatalogue.Items)  =
-    Seq.empty
-    |> getHeadphonesFromCatalogue storeItems.Headphones
-    |> getBooksFromCatalogue storeItems.Books
-    |> getComputersFromCatalogue storeItems.Computers
+    computers
+    |> Seq.append books
+    |> Seq.append headphones
