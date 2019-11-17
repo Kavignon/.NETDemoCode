a1b07d95228b35b94ca827b5584e3d505618f623
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-17 16:26:10 -0500

    Only loading headphones and books.

--- SAFE-Mock-Commerce/src/Server/CatalogApiWorkflows.fs
+++ SAFE-Mock-Commerce/src/Server/CatalogApiWorkflows.fs
@@ -1,13 +1,14 @@
 module CatalogApiWorkflows
 
-open Shared
+open Shared.CatalogueDto
 open DtoToDomainMapper
 
 let hydrateProductSeq (storeItems: ProductCatalogue.Items)  =
     let headphones = storeItems.Headphones |> Seq.map (HeadphoneDto >> convertDtoToStoreProduct)
-    let computers = storeItems.Computers |> Seq.map (ComputerDto >> convertDtoToStoreProduct)
     let books = storeItems.Books |> Seq.map (ReadingMaterialDto >> convertDtoToStoreProduct)
-
-    computers
-    |> Seq.append books
-    |> Seq.append headphones
+    (headphones, books) ||> Seq.append |> Seq.toList
+    // // let computers = storeItems.Computers |> Seq.map (ComputerDto >> convertDtoToStoreProduct)
+    // // computers
+    // // |> Seq.append books
+    // // |> Seq.append headphones
+    // // |> Seq.toList
