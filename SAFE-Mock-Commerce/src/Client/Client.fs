56ac82a806d2984878ad8edc7360a642d1c84683
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-17 16:28:13 -0500

    Put in place the infrastructure to support products loading from server.

--- SAFE-Mock-Commerce/src/Client/Client.fs
+++ SAFE-Mock-Commerce/src/Client/Client.fs
@@ -1,62 +1,43 @@
-module App
+module Client
 
 open Elmish
 open Elmish.React
 open Fable.React
 open Fable.React.Props
+open Fable.Remoting.Client
 open Fulma
 open Thoth.Json
 
 open Shared
+open Pages
 
-// The model holds data that you want to keep track of while the application is running
-// in this case, we are keeping track of a counter
-// we mark it as optional, because initially it will not be available from the client
-// the initial value will be requested from server
-type Model = { Counter: Counter option }
+type WebApplicateModel = {
+    Products: StoreProduct list
+}
 
-// The Msg type defines what events/actions can occur while the application is running
-// the state of the application changes *only* in reaction to these events
-type Msg =
-    | Increment
-    | Decrement
-    | InitialCountLoaded of Counter
+type EventMessage =
+    | FetchingProductsFromCatalogue
+    | LoadedProductsFromCatalogue of StoreProduct list
 
-module Server =
-
-    open Shared
-    open Fable.Remoting.Client
-
-    /// A proxy you can use to talk to server directly
-    let api : ICounterApi =
-      Remoting.createApi()
-      |> Remoting.withRouteBuilder Route.builder
-      |> Remoting.buildProxy<ICounterApi>
-let initialCounter = Server.api.initialCounter
+let remoteWebStoreApi =
+  Remoting.createApi()
+  |> Remoting.withRouteBuilder Route.builder
+  |> Remoting.buildProxy<MockStoreWebApi>
 
 // defines the initial state and initial command (= side-effect) of the application
-let init () : Model * Cmd<Msg> =
-    let initialModel = { Counter = None }
-    let loadCountCmd =
-        Cmd.OfAsync.perform initialCounter () InitialCountLoaded
-    initialModel, loadCountCmd
+let init () =
+    let initialModel = { Products = [] }
+    initialModel, Cmd.ofMsg FetchingProductsFromCatalogue
 
 // The update function computes the next state of the application based on the current state and the incoming events/messages
 // It can also run side-effects (encoded as commands) like calling the server via Http.
 // these commands in turn, can dispatch messages to which the update function will react.
-let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
-    match currentModel.Counter, msg with
-    | Some counter, Increment ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value + 1 } }
-        nextModel, Cmd.none
-    | Some counter, Decrement ->
-        let nextModel = { currentModel with Counter = Some { Value = counter.Value - 1 } }
-        nextModel, Cmd.none
-    | _, InitialCountLoaded initialCount->
-        let nextModel = { Counter = Some initialCount }
-        nextModel, Cmd.none
-    | _ -> currentModel, Cmd.none
-
+let update (msg : EventMessage) (currentModel : WebApplicateModel) : WebApplicateModel * Cmd<EventMessage> =
+    match currentModel, msg with
+    | _, FetchingProductsFromCatalogue ->
+        currentModel, Cmd.OfAsync.perform remoteWebStoreApi.fetchProducts () LoadedProductsFromCatalogue
+    | _, LoadedProductsFromCatalogue products ->
+        { currentModel with Products = products }, Cmd.none
 
 let safeComponents =
     let components =
@@ -83,9 +64,10 @@ let safeComponents =
           str " powered by: "
           components ]
 
-let show = function
-    | { Counter = Some counter } -> string counter.Value
-    | { Counter = None   } -> "Loading..."
+let show model =
+    match model with
+    | m when m.Products.Length > 0 -> sprintf "%A" model.Products
+    | _  -> "Loading..."
 
 let button txt onClick =
     Button.button
@@ -94,7 +76,7 @@ let button txt onClick =
           Button.OnClick onClick ]
         [ str txt ]
 
-let view (model : Model) (dispatch : Msg -> unit) =
+let view (model : WebApplicateModel) (dispatch : EventMessage -> unit) =
     div []
         [ Navbar.navbar [ Navbar.Color IsPrimary ]
             [ Navbar.Item.div [ ]
@@ -105,8 +87,10 @@ let view (model : Model) (dispatch : Msg -> unit) =
               [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
                     [ Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ] ]
                 Columns.columns []
-                    [ Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
-                      Column.column [] [ button "+" (fun _ -> dispatch Increment) ] ] ]
+                    [
+                        Column.column [] [ button "Hit Products Endpoint" (fun _ -> dispatch FetchingProductsFromCatalogue ) ]
+                        Column.column [] [ label [] [ str (sprintf "%A" model.Products)] ]
+                    ] ]
 
           Footer.footer [ ]
                 [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
