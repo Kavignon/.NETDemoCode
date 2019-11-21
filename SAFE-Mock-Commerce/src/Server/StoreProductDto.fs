ece710bc3428e3c44ffc4a7f95722ba24a06d3d3
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-20 23:25:13 -0500

    Linting the code.

--- SAFE-Mock-Commerce/src/Server/StoreProductDto.fs
+++ SAFE-Mock-Commerce/src/Server/StoreProductDto.fs
@@ -1,11 +1,12 @@
 module CatalogueDto
-    open FSharp.Data
 
-    type ProductCatalogue = XmlProvider<"./StoreProducts.xml"> // TODO: Enhancement - Move the data to a SQL db in Azure and load data from there.
+open FSharp.Data
 
-    let productsFromDatabase = ProductCatalogue.GetSample()
+type ProductCatalogue = XmlProvider<"./StoreProducts.xml"> // TODO: Enhancement - Move the data to a SQL db in Azure and load data from there.
 
-    type StoreProductDto =
-        | HeadphoneDto of ProductCatalogue.Headphone
-        | ReadingMaterialDto of ProductCatalogue.Book
-        | ComputerDto of ProductCatalogue.Computer
+let productsFromDatabase = ProductCatalogue.GetSample()
+
+type StoreProductDto =
+    | HeadphoneDto of ProductCatalogue.Headphone
+    | ReadingMaterialDto of ProductCatalogue.Book
+    | ComputerDto of ProductCatalogue.Computer
