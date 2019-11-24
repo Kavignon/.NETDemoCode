373776de6c3f15554a955b00c46821579061f6ed
Kevin Avignon <kevin.o.avignon@gmail.com>
2019-11-24 15:22:47 -0500

    Only one file for reusable common components
    The components are pretty small and it doesn't justify creating specific files for a reusable component.

--- SAFE-Mock-Commerce/src/Client/Components/Common/CommonReusableReactComponents.fs
+++ SAFE-Mock-Commerce/src/Client/Components/Common/CommonReusableReactComponents.fs
@@ -1,4 +1,4 @@
-module ProductImageComponent
+module CommonReactComponent
 
 open Feliz
 
@@ -11,4 +11,10 @@ let makeProductImage imageSourceLinkPath (width: int) (height: int) =
             style.height height
             style.paddingTop 35
         ]
+    ]
+
+let makePriceComponent amount =
+    Html.div [
+        prop.text (sprintf "Price: CDN$ %.2f" amount)
+        prop.style [ style.color "blue" ]
     ]
